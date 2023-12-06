8f8f3ee7d5675de12c9bcafcf1a55708e29e8e77
LEE GI UN <sunday6602@naver.com>
2023-12-06 17:13:16 +0900

    gu_19

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const TapMenu = document.querySelectorAll('.Reactions_Tap_Menu p')
+  const TapMenu = document.querySelector('.Reactions_Tap_Menu p')
 
-  TapMenu[1].style.backgroundColor = 'blue'
+  TapMenu.style.backgroundColor = 'blue'
+
 
   TapMenu.forEach((p, i) => {
     p[i].addEventListener('click', function() {
       for(let i = 0; TapMenu.length; i++) {
+
+        let tap = 1;
+
+        if (tap = 1) {
+          TapMenu[0].style.backgroundColor = 'red'
+          TapMenu[1].style.backgroundColor = 'blue'
+        } else if (tap = 0) {
+          TapMenu[1].style.backgroundColor = 'red'
+          TapMenu[0].style.backgroundColor = 'blue'
+        }
+
       }
     })
   })
